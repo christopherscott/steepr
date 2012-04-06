76a290be8bf21d68bd0af15ef9d0c14a227d6428
Chris Hernandez <c.scott.hernandez@gmail.com>
2012-04-06 09:56:54 -0400

    minor formatting tweak

--- js/steepr.main.js
+++ js/steepr.main.js
@@ -1,61 +1,44 @@
 (function() {
-  var Steepr,
-    __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+  var DEFAULT_TEAS, Steepr;
+
+  DEFAULT_TEAS = [
+    {
+      name: "White",
+      times: [60, 60, 90, 105],
+      temperature: "170-180",
+      count: 0
+    }, {
+      name: "Green",
+      times: [60, 60, 90, 105],
+      temperature: "170-180",
+      count: 0
+    }, {
+      name: "Oolong",
+      times: [30, 30, 45, 45],
+      temperature: "190-195",
+      count: 0
+    }, {
+      name: "Black",
+      times: [60, 60, 90, 90],
+      temperature: "212",
+      count: 0
+    }, {
+      name: "Pu-erh",
+      times: [30, 30, 45, 60],
+      temperature: "212",
+      count: 0
+    }
+  ];
 
   Steepr = window.Steepr || (window.Steepr = {
     models: {},
     collections: {},
     views: {},
     init: function(debug) {
+      this.teas = new this.collections.Teas(DEFAULT_TEAS);
       this.app = new this.views.Application;
       if (debug) return console.log("application started");
     }
   });
-
-  Steepr.views.Application = (function(_super) {
-
-    __extends(Application, _super);
-
-    function Application() {
-      Application.__super__.constructor.apply(this, arguments);
-    }
-
-    Application.prototype.initialize = function() {
-      new Swipe($("#types").get(0));
-      return $("#steep").on("pageshow", function(e, data) {
-        var steep;
-        console.log(e, data);
-        steep = $("#steep .content");
-        steep.addClass("brewing");
-        return setTimeout((function() {
-          steep.removeCass('brewing');
-          console.log('about to transition');
-          return $.mobile.changePage("#home", {
-            transition: "slide",
-            reverse: true
-          });
-        }), 9000);
-      });
-    };
-
-    Application.prototype.events = {
-      "click #steepit": "logit",
-      "pageshow #steep": "steep"
-    };
-
-    Application.prototype.render = function() {};
-
-    Application.prototype.logit = function() {
-      return console.log(arguments);
-    };
-
-    Application.prototype.steep = function() {
-      return console.log("steeping");
-    };
-
-    return Application;
-
-  })(Backbone.View);
 
 }).call(this);
